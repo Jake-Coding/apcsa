7c903ecaf533a314be7ed63e357adeee56a011c5
Jake Bridge <jakebridge2005@gmail.com>
2020-09-02 11:25:48 -0600

    avg rain done trig almost

---
+++ ProgrammedLessonExercises/Chapter10/AvgRain.java
@@ -0,0 +1,10 @@
+public class AvgRain {
+    public static void main(String[] args) {
+        double apr, may, june;
+        apr = 12;
+        may = 14;
+        june = 8;
+        double avg = (apr+may+june)/3;
+        System.out.println(avg);
+    }
+}
---
+++ ProgrammedLessonExercises/Chapter10/Trig.java
@@ -0,0 +1,8 @@
+public class Trig {
+    public static void main(String[] args) {
+        
+    }
+    public static double DtoR(double degrees) {
+        return degrees * Math.PI/180;
+    }
+}
