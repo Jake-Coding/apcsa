90efdb8131a30d4ce0ed9bd72552058d4f070517
Jake Bridge <34487865+Jake-Coding@users.noreply.github.com>
2020-10-09 10:59:12 -0600

    modified variable name -> more explicit

--- ProgrammedLessonExercises/Chapter25/TriSquare.java
+++ ProgrammedLessonExercises/Chapter25/TriSquare.java
@@ -2,11 +2,11 @@ public class TriSquare {
     public static void main(String[] args) {
         int upperBound = 50000;
         for (int i = 1; i < (int) (Math.sqrt(upperBound)); i++) {
-            int n = i * i;
+            int squareI = i * i;
             // System.out.println(i);
-            for (int j = 0; j <= n/2; j++) {
-                if (j*(j+1) / 2 == n) {
-                    System.out.println(n + " is both triangular and square");
+            for (int j = 0; j <= squareI/2; j++) {
+                if (j*(j+1) / 2 == squareI) {
+                    System.out.println(squareI + " is both triangular and square");
                 }
             }
         }
