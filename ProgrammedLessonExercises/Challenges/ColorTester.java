b2514b4245a07dfacd1e708c51b4344e5d8c83db
Jake Bridge <jakebridge2005@gmail.com>
2020-11-02 08:08:10 -0700

    added newlines in colortester -> more readable

--- ProgrammedLessonExercises/Challenges/ColorTester.java
+++ ProgrammedLessonExercises/Challenges/ColorTester.java
@@ -2,12 +2,16 @@ public class ColorTester {
     public static void main(String[] args) {
         Color firstColor = new Color(-1,55,257);
         System.out.println(firstColor); // [0,55,255]
+
         firstColor.setBlue(3);
         System.out.println(firstColor); // [0,55,3]
+
         firstColor.randomize();
         System.out.println(firstColor); // random ints
+
         firstColor.darken(0.1); 
         System.out.println(firstColor); // slightly lower values than above random ints
+
         Color hexColor = new Color("ffffff");
         System.out.println(hexColor); // [255,255,255]
 
