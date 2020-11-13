5d9f4aeb56ea41d634ccc9162234551e3acd684a
Jake Bridge <34487865+Jake-Coding@users.noreply.github.com>
2020-11-13 10:37:01 -0700

    fixed possible issue with resetting balance and 0 bonuses. Added vars to keep track of total spent and lunches

--- ProgrammedLessonExercises/Challenges/LunchAccount/LunchAccount.java
+++ ProgrammedLessonExercises/Challenges/LunchAccount/LunchAccount.java
@@ -4,6 +4,9 @@ public class LunchAccount {
     private static int studentsWithMoneyAdded = 0;
     private double balance;
     private String student;
+    private int lunchesBought;
+    private double totalSpent;
+    private boolean moneyAddedYet = false;
 
     public LunchAccount(String student, double balance) {
         this.balance = 0;
@@ -11,6 +14,8 @@ public class LunchAccount {
         this.student = student;
         addMoney(balance);
         globalIDCounter++;
+        lunchesBought = 0;
+        totalSpent = 0;
         
     }
 
@@ -20,11 +25,14 @@ public class LunchAccount {
 
     public double getBalance() {return balance;}
     public String getStudent() {return student;}
+    public int getLunchesBought() {return lunchesBought;}
+    public double getTotSpent() {return totalSpent;}
 
     public void addMoney(double money) {
         if (money > 0) {
-            if (this.balance == 0) {
+            if (!moneyAddedYet) {
                 studentsWithMoneyAdded++;
+                this.moneyAddedYet = true;
                 if (studentsWithMoneyAdded < 100) {
                     this.balance += 20;
                 }
@@ -36,13 +44,15 @@ public class LunchAccount {
     public boolean getLunch(double price) {
         if (price <= balance) {
             balance -= price;
+            lunchesBought++;
+            totalSpent += price;
             return true;
         }
         return false;
     }
 
     public String toString() {
-        return student + " #" + id + " has " + "$" + balance;
+        return student + " #" + id + " has " + "$" + balance +". They have bought " + lunchesBought + " lunches and have spent $" + totalSpent + " in total.";
     }
 
 
--- ProgrammedLessonExercises/Challenges/LunchAccount/LunchTester.java
+++ ProgrammedLessonExercises/Challenges/LunchAccount/LunchTester.java
@@ -4,20 +4,24 @@ public class LunchTester {
         for (int i = 0; i < 101; i++) {
             lunchAccounts[i] = new LunchAccount("ACCT"+i, 1);
         }
-        System.out.println(lunchAccounts[0]); // ACCT0 ID 1000 $21
-        System.out.println(lunchAccounts[100]); // ACCT100 ID 1100 $1
+        System.out.println(lunchAccounts[0]); // ACCT0 ID 1000 $21 overall spent $0 no lunches bought
+        System.out.println(lunchAccounts[100]); // ACCT100 ID 1100 $1 overall spent $0
 
         lunchAccounts[0].addMoney(30);
-        System.out.println(lunchAccounts[0]); // ACCT0 ID 1000 $51
+        System.out.println(lunchAccounts[0]); // ACCT0 ID 1000 $51 overall spent $0
 
         boolean gotLunch = lunchAccounts[100].getLunch(2);
         System.out.println(gotLunch); // false
-        System.out.println(lunchAccounts[100]); // ACCT100 ID 1100 $1
+        System.out.println(lunchAccounts[100]); // ACCT100 ID 1100 $1 overall spent $0
 
 
         gotLunch = lunchAccounts[0].getLunch(51);
         System.out.println(gotLunch); // true
-        System.out.println(lunchAccounts[0]); // ACCT0 ID 1000 $0
+        System.out.println(lunchAccounts[0]); // ACCT0 ID 1000 $51 overall spent $51 1 lunch bought.
+
+        lunchAccounts[0].addMoney(30);
+        lunchAccounts[0].getLunch(25);
+        System.out.println(lunchAccounts[0]); // Acct0 ID 1000 $76 overall 2 lunches bought
         
 
 
