4c99dd28fe49fc4a46c3504a1ac293bf2ba26ea0
Jake Bridge <34487865+Jake-Coding@users.noreply.github.com>
2020-11-30 11:15:47 -0700

    starting with IO

--- io/IO.java
+++ io/IO.java
@@ -3,5 +3,29 @@ import java.util.*;
 public class IO {
     public static void main(String[] args) {
 
+        File file;        
+        Scanner scanner;
+        ArrayList<Integer> data = new ArrayList<Integer>();
+
+        try {
+            file = new File("data.csv");
+            scanner = new Scanner(file);
+
+            while (scanner.hasNextInt()) {
+                data.add(scanner.nextInt());
+            }
+            System.out.println(data);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            System.exit(0);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }      
+
+
+
     }
 }
