b330559116b6fd98c17807f3d6076154b69aa26a
Jake Bridge <34487865+Jake-Coding@users.noreply.github.com>
2021-01-08 11:00:26 -0700

    commented code

--- algs/sortingAlgs.java
+++ algs/sortingAlgs.java
@@ -12,8 +12,10 @@ public class sortingAlgs {
     }
 
     public static int[] selectionSort(int[] data, int start) {
-        int minIndex = start;
+        // break condition
         if (start == data.length) {return data;}
+
+        int minIndex = start;
         // get min
         for (int i = start+1; i < data.length; i++) {
             if (data[i] < data[minIndex]) {minIndex = i;}
@@ -29,8 +31,11 @@ public class sortingAlgs {
     }
 
     public static int[] insertionSort(int[] data, int start) {
+        // break condition
         if (start == 0) {return data;}
+
         int minIndex = start;
+        // the swappening
         for (int i = start; i >= 0; i--) {
             if (data[i] > data[minIndex]) {
                 int temp = data[i];
@@ -38,14 +43,20 @@ public class sortingAlgs {
                 data[minIndex] = temp;
             }
         }
+
+        // r e c u r s i o n
         return insertionSort(data, start-1);
     }
 
     public static int[] bubbleSort(int[] data) {
+        // break conditions
         boolean noSwaps = false;
         int traversals = 0;
+
+
         while (!noSwaps) {
             noSwaps = true;
+            // the swappening
             for (int i = 0; i < data.length-1; i++) {
                 if (data[i] > data[i+1]) {
                     int temp = data[i];
@@ -58,6 +69,8 @@ public class sortingAlgs {
             traversals++;
             if (traversals == data.length) {break;}
         }
+
+        // no recursion ;-;
         return data;
  
     }
