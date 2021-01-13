f4b8a0d2e900e37c53e34da17ed9510cfc5f70f9
Jake Bridge <34487865+Jake-Coding@users.noreply.github.com>
2021-01-13 11:10:48 -0700

    multidim arrays

---
+++ Notes/MultiDimensional.java
@@ -0,0 +1,13 @@
+public class MultiDimensional {
+    public static void main(String[] args) {
+        boolean jan_13_stuff = true;
+        if (jan_13_stuff) {
+            int[][] matrix = new int[10][8];
+            for (int i = 0; i < matrix[0].length; i++) {
+                matrix[0][i]++; // add 1 to each element in the first row
+            }
+        }
+
+    }
+        
+}
