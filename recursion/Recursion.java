5ed0cc713d2684e879e6e23085abd3562e3fc286
Jake Bridge <34487865+Jake-Coding@users.noreply.github.com>
2021-02-18 11:43:40 -0700

    exercises from class 2/18

--- recursion/Recursion.java
+++ recursion/Recursion.java
@@ -1,6 +1,6 @@
 public class Recursion {
     public static void main(String[] args) {
-        System.out.println(fib(6));
+        System.out.println(reverse("apple"));
     }
 
     public static int iterativeFactorial(int n) {
@@ -19,5 +19,24 @@ public class Recursion {
     public static int fib(int n) {
         if (n < 1 ) {return 1;}
         return fib(n-1) + fib(n-2);
+    }
+
+    public static int vowelCount(String s) {
+        if (s.length() == 0) return 0;
+        if (isVowel(s.substring(0,1))) {
+            return 1 + vowelCount(s.substring(1));
+        }
+        return vowelCount(s.substring(1));
+
+    }
+
+    public static boolean isVowel(String s) {
+        if (s.length() != 1) return false;
+        return s.toLowerCase().matches("[aeiou]");
+    }
+
+    public static String reverse(String s) {
+        if (s.length() <= 1) return s;
+        return reverse(s.substring(1)) + s.substring(0, 1);
     }
 }
