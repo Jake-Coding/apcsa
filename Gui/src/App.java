d9980dfa94f7cd03b30de2c92e7581f24c5cbf80
Jake Bridge <34487865+Jake-Coding@users.noreply.github.com>
2021-02-26 10:47:21 -0700

    increment text num button

--- Gui/src/App.java
+++ Gui/src/App.java
@@ -1,7 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class App extends JFrame{
+public class App extends JFrame {
+
+    private JPanel panel;
+    private JLabel text;
+    private JButton button;
+    private JButton numButton;
+    private JLabel numText;
+    private int num;
     public static void main(String[] args) throws Exception {
         App app = new App();
         app.setVisible(true);
@@ -9,18 +17,45 @@ public class App extends JFrame{
 
     public App() {
         setTitle("Example app :)");
-        setSize(500,500);
+        setSize(640,480);
         setLocationRelativeTo(null); // centers
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        JPanel panel = new JPanel();
+        panel = new JPanel();
         panel.setLayout(null);
         add(panel);
 
-        JLabel text = new JLabel("Hello world", SwingConstants.CENTER);
+        text = new JLabel("Hello world", SwingConstants.CENTER);
         text.setFont(new Font("Serif", Font.BOLD, 20));
         text.setBounds(0,20,getBounds().width, 100);
         panel.add(text);
 
+        button = new JButton("Click me");
+        button.setBounds((int) getBounds().getWidth() / 2 - 100, 100, 200, 40);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                text.setText("get clicked lmao");
+            }
+        }); 
+        panel.add(button);
+
+        num = 0;
+        numText = new JLabel(String.valueOf(num), SwingConstants.CENTER);
+        numText.setFont(new Font("Serif", Font.BOLD, 20));
+        numText.setBounds(0, 100, getBounds().width, 100);
+
+        numButton = new JButton("increment");
+        numButton.setBounds(getBounds().width / 2-100, 200, 200, 40);
+        numButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                num++;
+                numText.setText(String.valueOf(num));
+            }
+        });
+        panel.add(numText);
+        panel.add(numButton);
     }
+
 }
